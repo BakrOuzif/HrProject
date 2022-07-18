7334fb99044a7f69cb531f86ebc0551cdf90ca8b
AnasFarhane <76697224+AnasFarhane@users.noreply.github.com>
2022-07-18 10:55:27 +0100

    Commit Adding Collab works fine little changes : Local dates back etc

---
+++ src/main/java/com/example/hrproject/modals/Status.java
@@ -0,0 +1,13 @@
+package com.example.hrproject.modals;
+
+public enum Status {
+    Saved("Saved","Entity has been saved"),
+    Deleted("Deleted","Entity has been deleted");
+    final String label;
+    final String description;
+
+    Status(String label, String description) {
+        this.label = label;
+        this.description = description;
+    }
+}
